7c88ecc37a2caa79801d6488905660d793056e95
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 20:11:31 -0300

    Calculadora versão 2 Soma e subtração

--- Calc.py
+++ Calc.py
@@ -3,4 +3,9 @@ print("Calculadora em 4 passos")
 print("------------------------")
 num= int (input("Digite o Primeiro numero : "))
 num1= int (input("Digite o Segundo numero : "))
-print("A soma desses dois numeros é de : ",num+num1)
+esc= int  (input("Escolha um numero de 1 a 4 : "))
+
+if esc==1 :
+  print("A soma desses dois numeros é de : ",num+num1)
+elif esc==2 :
+  print("A subtração desses dois numeros é de : ",num-num1) 
